94b77bd4cf8c3e5c5808492ffbedd067938721a0
Evgeniya Potar <zhenik1303@gmail.com>
2025-08-25 11:36:59 +0300

    moved prettier files to the root of the project, change components: App, List

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useRef, useState} from 'react'
+import {useRef, useState} from 'react'
 import './App.css'
 import List from "./Components/List.jsx";
 
@@ -26,10 +26,37 @@ function App() {
       },
   ])
 
+    const inputRef = useRef()
+
+    const changeProduct = (id) =>{
+        const newProducts = listProducts.map((item) => {
+            if (item.id === id) {
+                item.title = '!!!' + item.title;
+            }
+            return item;
+        });
+        setListProducts(newProducts);
+    }
+
+    const focusInput = () => {
+        inputRef.current.focus()
+    }
+
+
+    const addProduct = (e) => {
+        if (e.key === "Enter" && e.target.value.trim() !== '') {
+            setListProducts([...listProducts, {id: listProducts.length + 1, title: e.target.value}])
+            e.target.value = ""
+        }
+    }
+
   return (
-    <>
-      <List listProducts = {listProducts} />
-    </>
+      <>
+          <h2>Список продуктов</h2>
+          <input ref={inputRef} type="text"  placeholder="Введите текст и нажмите Enter" onKeyDown={e => addProduct(e)}/>
+          <button onClick={focusInput}>Фокус</button>
+          <List products = {listProducts} onChangeProduct={changeProduct} />
+      </>
   )
 }
 
